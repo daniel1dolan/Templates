0f182035f9f6958938d0d054f76589db560aa844
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-04-10 13:50:00 -0500

    added localstorage

--- redux-template/src/index.js
+++ redux-template/src/index.js
@@ -16,11 +16,44 @@ import Container from "./components/Container";
 //Root Reducer
 import rootReducer from "./reducers/rootReducer";
 
+//Local storage
+let saveToLocalStorage = state => {
+  try {
+    const serializeState = JSON.stringify(state);
+    localStorage.setItem("state", serializeState);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+let loadFromLocalStorage = () => {
+  try {
+    const serializeState = localStorage.getItem("state");
+    if (serializeState === null) {
+      return undefined;
+    } else {
+      return JSON.parse(serializeState);
+    }
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+};
+
+//Loads data from storage
+const persistedState = loadFromLocalStorage();
+
 //Redux Store
 let store = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+//Connects local storage function to redux store.
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
+});
 
 ReactDOM.render(
   <Provider store={store}>
